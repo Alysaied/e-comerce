3d811ba57d6355d2e111d06a54a4032166cc77d4
Alysaied <alysaied461@gmail.com>
2024-07-12 17:19:25 -0700

    implement carts services

---
+++ front-end/src/app/services/carts/carts.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {environment} from "../../../environments/environment";
+import {HttpClient, HttpParams} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartsService {
+
+  constructor(private http:HttpClient) { }
+  // createNewCart(model:any){
+  //   return this.http.post(environment.baseApi+'carts',model)
+  // }
+  getAllCarts(param?:any){
+    let params=new HttpParams()
+    params=params.append("startDate",param?.start).append("endDate",param?.end)
+    return this.http.get(environment.baseApi+'carts',{params})
+  }
+  deleteCart(id:number){
+    return this.http.delete(environment.baseApi+'carts/'+id)
+  }
+}
